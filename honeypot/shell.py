b9e30c91cff9adffa96e3b8a6e4a80e9fe5d8588
Jainam maru <157703342+Dev9269@users.noreply.github.com>
2025-07-26 11:11:18 +0530

    fix readme installation instructions

--- honeypot/shell.py
+++ honeypot/shell.py
@@ -100,7 +100,6 @@ def _get_fake_listing(path: str) -> str:
             current = children[part]
         else:
             return f'ls: cannot access {path}: No such file or directory\r\n'
-
     if current.get('type') != 'dir':
         return f'{path}\r\n'
 
