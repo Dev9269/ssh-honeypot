c814789c6ef501f69bea6d516f9d59f165dffebe
Jainam maru <157703342+Dev9269@users.noreply.github.com>
2025-07-29 09:46:39 +0530

    handle log file permission errors

--- honeypot/ai_engine.py
+++ honeypot/ai_engine.py
@@ -29,7 +29,6 @@ class SessionState:
         self.history.append(cmd)
         if len(self.history) > config.AI_MAX_HISTORY:
             self.history = self.history[-config.AI_MAX_HISTORY:]
-
     def get_context(self) -> str:
         if not self.history:
             return ''
