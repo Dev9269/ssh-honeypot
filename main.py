349519a43516ac9969f1fda73ff681772d3100b4
Jainam maru <157703342+Dev9269@users.noreply.github.com>
2025-09-08 16:12:21 +0530

    add env var override for config values

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ Examples:
     parser.add_argument('--whitelist', default=None, help='Comma separated IPs/CIDRs to allow')
     parser.add_argument('--version', action='version', version='SSH Honeypot v2.0.0')
     args = parser.parse_args()
-
     if args.config:
         config.load_yaml_config(args.config)
 
